acd3aa7d9ad84b1e1cf5c8b2a7d6d0a95f3465fb
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2024-01-12 14:02:24 +0300

    New exersizes

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ api_v1_urlpatterns = [
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('exersizes', include('exersizes.urls')),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
---
+++ exersizes/tests/test_hello_view.py
@@ -0,0 +1,10 @@
+import pytest
+
+pytestmark = [pytest.mark.django_db]
+
+
+def test_hello(client):
+    got = client.get('/exersizes/hello')
+
+    assert got.status_code == 200
+    assert got.content == b'<h1>Hello, world</h1>'
